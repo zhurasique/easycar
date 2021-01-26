9e286af2c88c2861268743feaaeeb2a674182848
Serhii Zhura <berezovsky.serega@gmail.com>
2021-01-26 16:26:19 +0200

    Delete base-service directory

--- base-service/src/main/java/com/service/base/repository/ImageRepo.java
+++
@@ -1,7 +0,0 @@
-package com.service.base.repository;
-
-import com.service.base.model.Image;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ImageRepo extends MongoRepository<Image, String> {
-}
