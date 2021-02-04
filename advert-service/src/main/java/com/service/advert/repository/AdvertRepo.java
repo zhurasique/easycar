b5d01bdb96a2884b5322f94a691098d81e8ee280
Serhii <zhuraserhii@gmail.com>
2021-02-04 13:24:04 +0200

    global refactoring

--- advert-service/src/main/java/com/service/advert/repository/AdvertRepo.java
+++
@@ -1,7 +0,0 @@
-package com.service.advert.repository;
-
-import com.service.advert.model.Advert;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface AdvertRepo extends MongoRepository<Advert, String> {
-}
--- base-service/src/main/java/com/service/base/repository/TypeRepo.java
+++
@@ -1,8 +0,0 @@
-package com.service.base.repository;
-
-import com.service.base.model.Type;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface TypeRepo extends MongoRepository<Type, String> {
-    Type findByName(String name);
-}
