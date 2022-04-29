b9527d02f1613d334057ae35eca17ef27767076c
zhurasique <zhuraserhii@gmail.com>
2022-04-29 11:31:42 +0300

    api standards symbols

--- base-service/src/main/java/com/service/base/controller/TypeModelController.java
+++ base-service/src/main/java/com/service/base/controller/TypeModelController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("api/base-service/type_model")
+@RequestMapping("api/base-service/type-model")
 public class TypeModelController {
 
     private final TypeModelService typeModelService;
