7c1e0b1d70a37c958f76a21b11eb17fe36befac3
zhurasique <zhuraserhii@gmail.com>
2022-04-30 18:25:32 +0300

    advice refactoring

--- base-service/src/main/java/com/service/base/advice/RestExceptionHandler.java
+++ base-service/src/main/java/com/service/base/advice/RestExceptionHandler.java
@@ -4,10 +4,10 @@ import lombok.Builder;
 import lombok.Data;
 import org.json.JSONException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.io.IOException;
@@ -18,56 +18,46 @@ import java.util.Map;
 public class RestExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Response> handleValidationExceptions(
-            MethodArgumentNotValidException ex) {
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public Response handleValidationExceptions(MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
         ex.getBindingResult().getAllErrors().forEach((error) -> {
             String fieldName = ((FieldError) error).getField();
             String errorMessage = error.getDefaultMessage();
             errors.put(fieldName, errorMessage);
         });
-        HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
-        Response response = Response.builder()
-                .status(httpStatus.value())
+        return Response.builder()
                 .message(errors.toString())
                 .build();
-        return new ResponseEntity<>(response, httpStatus);
     }
 
     @ExceptionHandler(IOException.class)
-    public ResponseEntity<Response> handleIoException(IOException ex) {
-        HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
-        Response response = Response.builder()
-                .status(httpStatus.value())
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public Response handleIoException(IOException ex) {
+        return Response.builder()
                 .message(ex.getMessage())
                 .build();
-        return new ResponseEntity<>(response, httpStatus);
     }
 
     @ExceptionHandler(JSONException.class)
-    public ResponseEntity<Response> handleJsonException(JSONException ex) {
-        HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
-        Response response = Response.builder()
-                .status(httpStatus.value())
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public Response handleJsonException(JSONException ex) {
+        return Response.builder()
                 .message(ex.getMessage())
                 .build();
-        return new ResponseEntity<>(response, httpStatus);
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<Response> handleException(Exception ex) {
-        HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
-        Response response = Response.builder()
-                .status(httpStatus.value())
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public Response handleException(Exception ex) {
+        return Response.builder()
                 .message(ex.getMessage())
                 .build();
-        return new ResponseEntity<>(response, httpStatus);
     }
 
     @Data
     @Builder
     private static class Response {
-        private int status;
         private String message;
     }
 }
--- image-service/src/main/java/com/service/image/advice/RestExceptionHandler.java
+++ image-service/src/main/java/com/service/image/advice/RestExceptionHandler.java
@@ -3,10 +3,10 @@ package com.service.image.advice;
 import lombok.Builder;
 import lombok.Data;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.io.IOException;
@@ -17,46 +17,38 @@ import java.util.Map;
 public class RestExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Response> handleValidationExceptions(
-            MethodArgumentNotValidException ex) {
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public Response handleValidationExceptions(MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
         ex.getBindingResult().getAllErrors().forEach((error) -> {
             String fieldName = ((FieldError) error).getField();
             String errorMessage = error.getDefaultMessage();
             errors.put(fieldName, errorMessage);
         });
-        HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
-        Response response = Response.builder()
-                .status(httpStatus.value())
+        return Response.builder()
                 .message(errors.toString())
                 .build();
-        return new ResponseEntity<>(response, httpStatus);
     }
 
     @ExceptionHandler(IOException.class)
-    public ResponseEntity<Response> handleIoException(IOException ex) {
-        HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
-        Response response = Response.builder()
-                .status(httpStatus.value())
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public Response handleIoException(IOException ex) {
+        return Response.builder()
                 .message(ex.getMessage())
                 .build();
-        return new ResponseEntity<>(response, httpStatus);
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<Response> handleException(Exception ex) {
-        HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
-        Response response = Response.builder()
-                .status(httpStatus.value())
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public Response handleException(Exception ex) {
+        return Response.builder()
                 .message(ex.getMessage())
                 .build();
-        return new ResponseEntity<>(response, httpStatus);
     }
 
     @Data
     @Builder
     private static class Response {
-        private int status;
         private String message;
     }
 }
