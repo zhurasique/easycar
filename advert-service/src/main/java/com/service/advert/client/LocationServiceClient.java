1c187729e1459a58a620deb6040a688682676acc
zhurasique <zhuraserhii@gmail.com>
2022-05-16 20:00:00 +0300

    remove unused import

--- advert-service/src/main/java/com/service/advert/client/LocationServiceClient.java
+++ advert-service/src/main/java/com/service/advert/client/LocationServiceClient.java
@@ -3,7 +3,6 @@ package com.service.advert.client;
 import com.service.advert.vo.Location;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
