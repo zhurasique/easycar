90054782a98557fa9ddb8ab775772aa5b9d7dd69
zhurasique <zhuraserhii@gmail.com>
2023-12-03 15:18:19 +0200

    Update web configuration

--- account-service/src/main/java/com/easycar/account/config/WebConfig.java
+++ account-service/src/main/java/com/easycar/account/config/WebConfig.java
@@ -3,8 +3,17 @@ package com.easycar.account.config;
 import com.easycar.common.config.ResourceServerConfig;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
 @Configuration
 @Import({ResourceServerConfig.class})
-public class WebConfig {
+public class WebConfig extends ResourceServerConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated();
+    }
 }
--- common/src/main/java/com/easycar/common/config/ResourceServerConfig.java
+++ common/src/main/java/com/easycar/common/config/ResourceServerConfig.java
@@ -8,18 +8,16 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.security.oauth2.client.feign.OAuth2FeignRequestInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.client.DefaultOAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsResourceDetails;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
 
 @Configuration
 @EnableResourceServer
 @RequiredArgsConstructor
-public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+public class ResourceServerConfig {
 
     private final ResourceServerProperties sso;
 
@@ -42,12 +40,5 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     @Bean
     public ResourceServerTokenServices tokenServices() {
         return new DetailedUserInfoTokenServices(sso.getUserInfoUri(), sso.getClientId());
-    }
-
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/").permitAll()
-                .anyRequest().authenticated();
     }
 }
